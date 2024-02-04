698729918a28740c8770b72c3c3c65057bac395a
alkhimovich_m <mikha.alkhimovich@mail.ru>
2024-02-05 01:02:39 +0700

    main: закончил 2-ую

--- 2-lab/src/bmp_createFrame.c
+++ 2-lab/src/bmp_createFrame.c
@@ -10,40 +10,33 @@ void AddRandomBorder(BMP_File* pBmpFile)
 {
 	srand(time(NULL));
 
-	int originalWidth = pBmpFile->m_dibHeader.m_width;
-	int originalHeight = pBmpFile->m_dibHeader.m_height;
+	int rowSize = (pBmpFile->m_dibHeader.m_width * pBmpFile->m_dibHeader.m_bitsPerPixel + 7) / 8;
+	int rowPadding = (4 - (rowSize % 4)) % 4; // Дополнительный байт для выравнивания
 
-	int rowSize = (originalWidth * pBmpFile->m_dibHeader.m_bitsPerPixel + 7) / 8;
-	int rowPadding = (4 - (rowSize % 4)) % 4;
+	unsigned char* newData = (unsigned char*)malloc(rowSize * pBmpFile->m_dibHeader.m_height);
 
-	unsigned char* newData = (unsigned char*)malloc(rowSize * originalHeight);
-
-	for (int i = 0; i < originalHeight; i++)
+	for (int i = 0; i < pBmpFile->m_dibHeader.m_height; i++)
 	{
-		for (int j = 0; j < originalWidth; j++)
+		for (int j = 0; j < pBmpFile->m_dibHeader.m_width; j++)
 		{
-		int originalOffset = i * rowSize + j * pBmpFile->m_dibHeader.m_bitsPerPixel / 8;
-		int newOffset = i * rowSize + j * pBmpFile->m_dibHeader.m_bitsPerPixel / 8;
+			int offset = i * rowSize + j * pBmpFile->m_dibHeader.m_bitsPerPixel / 8;
 
-		if (j < 15 || j >= originalWidth - 15 || i < 15 || i >= originalHeight - 15)
-		{
-			for (int k = 0; k < pBmpFile->m_dibHeader.m_bitsPerPixel / 8; k++)
+			if (j < 15 || j >= pBmpFile->m_dibHeader.m_width - 15 || i < 15 || i >= pBmpFile->m_dibHeader.m_height - 15)
 			{
-				newData[newOffset + k] = rand() % 256;
+				for (int k = 0; k < pBmpFile->m_dibHeader.m_bitsPerPixel / 8; k++)
+				{
+					newData[offset + k] = rand() % 256;
+				}
 			}
-		}
-		else
-		{
-			for (int k = 0; k < pBmpFile->m_dibHeader.m_bitsPerPixel / 8; k++)
+			else
 			{
-				newData[newOffset + k] = pBmpFile->m_pData[originalOffset + k];
+				for (int k = 0; k < pBmpFile->m_dibHeader.m_bitsPerPixel / 8; k++)
+				{
+					newData[offset + k] = pBmpFile->m_pData[offset + k];
+				}
 			}
-		}
 		}
 	}
-
-	pBmpFile->m_dibHeader.m_width = originalWidth;
-	pBmpFile->m_dibHeader.m_height = originalHeight;
 
 	free(pBmpFile->m_pData);
 	pBmpFile->m_pData = newData;
